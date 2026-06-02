139fa4f9612df63498f94128de5d8c05eb8b0657
abdulmazith-coder <abdul.mazith.21@gmail.com>
2026-06-02 10:06:42 +0530

    update the news backend

--- main/settings.py
+++ main/settings.py
@@ -62,6 +62,11 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
+CORS_ALLOWED_ORIGINS = [
+    "https://stocbuy.pages.dev", 
+]
+
+
 CORS_ALLOW_ALL_ORIGINS = True
 
 ROOT_URLCONF = 'main.urls'
