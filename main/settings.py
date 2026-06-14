e255417e987091321e23b8babc13f30b4b57f116
abdulmazith-coder <abdul.mazith.21@gmail.com>
2026-06-14 17:39:00 +0530

    update the backend

--- main/settings.py
+++ main/settings.py
@@ -157,7 +157,7 @@ REST_FRAMEWORK = {
 }
 
 SIMPLE_JWT = {
-    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=60),
+    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=2),
     'REFRESH_TOKEN_LIFETIME': timedelta(days=30),
     'AUTH_HEADER_TYPES': ('Bearer',),
     'ROTATE_REFRESH_TOKENS': True,
